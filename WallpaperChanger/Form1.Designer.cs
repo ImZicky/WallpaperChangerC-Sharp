b5424df5fea9fb9a736a45f09097fa29408f5ee3
= <=>
2019-04-15 15:42:12 -0300

    first commit

---
+++ WallpaperChanger/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WallpaperChanging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 42);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 22);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "mudar";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(103, 47);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(16, 13);
+            this.labelFileName.TabIndex = 1;
+            this.labelFileName.Text = "...";
+            this.labelFileName.Click += new System.EventHandler(this.labelFileName_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(401, 107);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "BackgroundChanger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelFileName;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WallpaperChanger/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WallpaperChanging
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string FileName;
+
+        const int SetDeskWallpaper = 20;
+        const int UpdateIniFile = 0x01;
+        const int SendWinIniChange = 0x02;
+        int index = 0;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public void Set()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+            SystemParametersInfo(SetDeskWallpaper, 0, FileName, UpdateIniFile | SendWinIniChange);
+        }
+
+        private void UpdateWallpaper()
+        {
+            string path = @"C:\Users\T-Gamer\Pictures\walpapers-sao";
+
+            var directory = new DirectoryInfo(path);
+            int totalFiles = directory.GetFiles().Count<FileInfo>();
+            var fileName = (from f in directory.GetFiles() orderby f.LastWriteTime descending select f).First<FileInfo>();
+            if (index < totalFiles)
+            {
+                fileName = (from f in directory.GetFiles() orderby f.LastWriteTime descending select f).ElementAt<FileInfo>(index);
+                index++;
+            }
+            else {
+                index = 0;
+            }
+
+
+            FileName = @"C:\Users\T-Gamer\Pictures\walpapers-sao\" + fileName;
+
+            labelFileName.Text = FileName;
+
+            Set();
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            UpdateWallpaper();
+        }
+
+        private void labelFileName_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateWallpaper();
+        }
+    }
+}
